36bc6c20e17c4c0cc1226bcf81d675600bf19c14
Tao Song <ts2695@columbia.edu>
2011-12-02 17:38:55 -0500

    lower bond check

--- MirrorUniverse/src/mirroruniverse/stupidplayer/LowerBoundDetecter.java
+++ MirrorUniverse/src/mirroruniverse/stupidplayer/LowerBoundDetecter.java
@@ -1,109 +1,89 @@
-package mirroruniverse.g3player;
+package mirroruniverse.stupidplayer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-import org.jgrapht.alg.*;
-import org.jgrapht.*;
-import org.jgrapht.graph.*;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 
-import mirroruniverse.sim.Player;
-
-public class G3Player implements Player {
-
-	public static final int U = 3, D = 7, R = 1, L = 5, LU = 4, RU = 2, LD = 6,
-			RD = 8;
-	public static final boolean printGraph = false;
-	public static final int SENTINEL = -1;
+public class LowerBoundDetecter {
+	public static final int BOND = 8;
+	public static final int MAXSIZE = (BOND+1)*2+1;
+	private int[][] localLeftMap;
+	private int[][] localRightMap;
+	public static final boolean printGraph = true;
 
 	private DefaultDirectedWeightedGraph<PointPair, SimpleEdge> graph;
-	private int round;
-
+	
 	private PointPair exit;
 	private PointPair start;
 	private PointPair source = null;
 	private Point startLeft = null;
 	private Point startRight = null;
 	private PointPair[][][][] pc;
-	
-	private static final Point PSENTINEL = new Point(-1,-1);
-	private static final PointPair UNEXPLORED = new PointPair(-1,-1,-1,-1);
-	private PointPair[][][][] bfsPath;
 	List<SimpleEdge> path;
 	SimpleEdge e;
-
-	public G3Player() {
+	ArrayList<PointPair> out;
+	
+	public LowerBoundDetecter(){
+		localLeftMap = new int[MAXSIZE][MAXSIZE];
+		localRightMap = new int[MAXSIZE][MAXSIZE];
+		pc = new PointPair[MAXSIZE][MAXSIZE][MAXSIZE][MAXSIZE];
+		out = new ArrayList<PointPair>();
+	}
+	
+	public int lowerBond(int[][] leftMap, int[][] rightMap, int leftExitx, int leftExity, int rightExitx, int rightExity){
 		graph = new DefaultDirectedWeightedGraph<PointPair, SimpleEdge>(
 				SimpleEdge.class);
-		round = 0;
-	}
-
-	@Override
-	public int lookAndMove(int[][] aintViewL, int[][] aintViewR) {
-		round++;
-		if (round==1) {
-			pc = new PointPair[aintViewL.length][aintViewL[0].length][aintViewR.length][aintViewR[0].length];
-			bfsPath = new PointPair[aintViewL.length][aintViewL[0].length][aintViewR.length][aintViewR[0].length];
-			buildGraph(aintViewL, aintViewR);
-//			System.out.println(graph);
-		}
-
-		path = DijkstraShortestPath.findPathBetween(graph, start, exit);// just
-																		// for
-																		// demo,
-																		// not
-																		// efficient
+		buildLocalMap(leftMap, rightMap, leftExitx, leftExity, rightExitx, rightExity);
+		buildGraph(localLeftMap,localRightMap);
+		PointPair exit = pc[BOND+1][BOND+1][BOND+1][BOND+1];
+		PointPair start = pc[0][0][0][0];
+		path = DijkstraShortestPath.findPathBetween(graph, start, exit);
 
 		System.out.println("=====path==========" + path);
-		// so far, the path is found
-		// but the getSource() function is protected
-
-		PointPair source = path.get(round-1).getFrom();
-		System.out.println("source "+source);
-		PointPair target = path.get(round-1).getTo();
-		System.out.println("target "+target);
-		int dx = target.getLeftx() - source.getLeftx();
-		dx = dx == 0 ? target.getRightx() - source.getRightx() : dx;
-		int dy = target.getLefty() - source.getLefty();
-		dy = dy == 0 ? target.getRighty() - source.getRighty() : dy;
-		if (dx == 1 && dy == 1) {
-			System.out.println("RD");
-			return RD;
-		} else if (dx == 1 && dy == -1) {
-			System.out.println("LU");
-			return LD;
-		} else if (dx == -1 && dy == 1) {
-			System.out.println("RU");
-			return RU;
-		} else if (dx == -1 && dy == -1) {
-			System.out.println("LU");
-			return LU;
+		int sum = 0;
+		for(SimpleEdge e : path){
+			sum+=e.getWeight();
 		}
-		if (dy == 1) {
-			System.out.println("L");
-			return R;
-		}
-		if (dy == -1) {
-			System.out.println("R");
-			return L;
-		}
-		if (dx == 1) {
-			System.out.println("D");
-			return D;
-		}
-		if (dx == -1) {
-			System.out.println("U");
-			return U;
-		}
-		//
-		return -1;
+		return sum;
 	}
+	
+	public static void main(String[] args) {
+		LowerBoundDetecter lb = new LowerBoundDetecter();
+		int[][] leftMap = {{1,1,1},{1,2,0},{0,0,0}};
+		System.out.println(leftMap.length);
+		int[][] rightMap = {{1,0,1},{1,2,1},{1,1,1}};
+		int diff = lb.lowerBond(leftMap, rightMap, 1, 1, 1, 1);
+		System.out.println("diff = " + diff);
+	}
+
+	private void buildLocalMap(int[][] leftMap, int[][] rightMap,int leftExitx, int leftExity, int rightExitx, int rightExity) {
+		out.clear();
+		for(int x = -BOND; x<=BOND;x++){
+			for(int y = -BOND;y<=BOND;y++){
+				if((x+leftExitx)<leftMap.length && (y+leftExity)<leftMap[0].length && (x+leftExitx)>=0 && (y+leftExity)>=0)
+					localLeftMap[x+BOND+1][y+BOND+1] = leftMap[x+leftExitx][y+leftExity]==1?1:0;
+				else
+					localLeftMap[x+BOND+1][y+BOND+1] = 0;
+				if(x+rightExitx<rightMap.length&&y+rightExity<rightMap[0].length && (x+rightExitx)>=0 && (y+rightExity)>=0)
+					localRightMap[x+BOND+1][y+BOND+1] = rightMap[x+leftExitx][y+leftExity]==1?1:0;
+				else
+					localRightMap[x+BOND+1][y+BOND+1] = 0;
+			}
+		}
+		localLeftMap[BOND+1][BOND+1] = 2;
+		localRightMap[BOND+1][BOND+1] = 2;
+				
+	}
+	
 
 	private int incr(int len, int x, int deltaX) {
 		if (deltaX > 0) {
-			return x + deltaX < len ? x + deltaX : SENTINEL;
+			return x + deltaX < len ? x + deltaX : x;
 		} else {
-			return x + deltaX > 0 ? x + deltaX : SENTINEL;
+			return x + deltaX > 0 ? x + deltaX : x;
 		}
 
 	}
@@ -111,9 +91,6 @@ public class G3Player implements Player {
 	private Point getNextPoint(int[][] arr, int x, int y, int deltaX, int deltaY) {
 		int xprime = incr(arr.length, x, deltaX);
 		int yprime = incr(arr[0].length, y, deltaY);
-		if(xprime == SENTINEL || yprime == SENTINEL){
-			return PSENTINEL;
-		}
 		return (arr[xprime][yprime] == 1) ? new Point(x, y) : new Point(xprime,
 				yprime);
 	}
@@ -122,18 +99,12 @@ public class G3Player implements Player {
 			int deltaY, int[][] left, int[][] right) {
 		Point leftPoint = getNextPoint(left, lx, ly, deltaX, deltaY);
 		Point rightPoint = getNextPoint(right, rx, ry, deltaX, deltaY);
-		
-		if(leftPoint == PSENTINEL || rightPoint == PSENTINEL){
-			return UNEXPLORED;
-		}
 		return new PointPair(leftPoint.getX(), leftPoint.getY(),
 				rightPoint.getX(), rightPoint.getY());
 	}
 
 	private void buildGraph(int[][] aintViewL, int[][] aintViewR) {
 		int lx, ly, rx, ry;
-		// add vertex
-		// flag accepting state if one exists
 		if (printGraph) {
 			System.out.println("Left View:");
 			for (int i = 0; i < aintViewL.length; i++) {
@@ -156,12 +127,12 @@ public class G3Player implements Player {
 					for (ry = 0; ry < aintViewR[0].length; ry++) {
 						if(aintViewL[lx][ly]!=1&&aintViewR[rx][ry]!=1){
 							pc[lx][ly][rx][ry] = new PointPair(lx, ly, rx, ry);
-						
+
 							if (printGraph)
 								System.out.println(pc[lx][ly][rx][ry] + "\t("
 										+ aintViewL[lx][ly] + ","
 										+ aintViewR[rx][ry] + ")");
-						
+
 							this.graph.addVertex(pc[lx][ly][rx][ry]);
 						}
 						if (aintViewL[lx][ly] == 2 && aintViewR[rx][ry] == 2) {
@@ -172,14 +143,17 @@ public class G3Player implements Player {
 				}
 			}
 		}
-		this.start = pc[aintViewL.length / 2][aintViewL[0].length / 2][aintViewR.length / 2][aintViewR[0].length / 2];
-		System.out.println("=====start========" + this.start);
 
 		// add edges
 		for (lx = 0; lx < aintViewL.length; lx++) {
 			for (ly = 0; ly < aintViewL[0].length; ly++) {
 				for (rx = 0; rx < aintViewR.length; rx++) {
 					for (ry = 0; ry < aintViewR[0].length; ry++) {
+						
+						// record all the edge vertices to a set
+						if(lx==0 || lx==MAXSIZE-1 || ly==0 || ly==MAXSIZE-1)
+							if(rx==0 || rx==MAXSIZE-1 || ry==0 || ry==MAXSIZE-1)
+								out.add(pc[lx][ly][rx][ry]);
 
 						if (aintViewL[lx][ly] == 1 || aintViewR[rx][ry] == 1)
 							continue;
@@ -197,7 +171,8 @@ public class G3Player implements Player {
 													.getLefty()][newState
 													.getRightx()][newState
 													.getRighty()]);
-										    graph.setEdgeWeight(e, 1);
+										    if(e!=null)
+										    	graph.setEdgeWeight(e, 0);
 									}
 									else{
 										if(aintViewL[lx][ly]==2){
@@ -214,19 +189,29 @@ public class G3Player implements Player {
 													.getLefty()][newState
 													.getRightx()][newState
 													.getRighty()]);
-										    graph.setEdgeWeight(e, 1000);
+										    if(e!=null)
+										    	graph.setEdgeWeight(e, 1);
 										}
 									}
 								}
 							}
 						}
-
-
 					}
 				}
 			}
 		}
+		//find u!
+		
+		for(int i =0; i<out.size()-1;i++){
+			e=graph.addEdge(out.get(i), out.get(i+1));
+			if(e!=null)
+				graph.setEdgeWeight(e, 0);
+		}
+		e=graph.addEdge(out.get(out.size()-1), out.get(0));
+		if(e!=null)
+			graph.setEdgeWeight(e, 0);
 
 	}
+	
 
 }
---
+++ MirrorUniverse/src/mirroruniverse/stupidplayer/SimpleEdge.java
@@ -0,0 +1,21 @@
+package mirroruniverse.stupidplayer;
+
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+@SuppressWarnings("serial")
+public class SimpleEdge extends DefaultWeightedEdge {
+	
+	public PointPair getFrom(){
+		return (PointPair) getSource();
+	}
+	
+	public PointPair getTo(){
+		return (PointPair) getTarget();
+	}
+	
+	public double getWeight(){
+		return super.getWeight();
+	}
+
+}
