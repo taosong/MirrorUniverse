73c6cc1a4774e49fc6a8afcf288d60452969e2fe
Neil Deshpande <neil.deshpa@gmail.com>
2011-12-04 15:38:56 -0500

    Tweaked the MinHeap to be more memory efficient

--- MirrorUniverse/src/mirroruniverse/stupidplayer/BinaryMinHeap.java
+++ MirrorUniverse/src/mirroruniverse/stupidplayer/BinaryMinHeap.java
@@ -1,23 +1,28 @@
 package mirroruniverse.stupidplayer;
 
+import gnu.trove.list.TIntList;
+import gnu.trove.list.linked.TIntLinkedList;
+import gnu.trove.map.hash.TIntIntHashMap;
+
 import java.util.ArrayList;
 
 public class BinaryMinHeap {
 	public static final int INF = Integer.MAX_VALUE;
 	
-	public ArrayList<Element> a = new ArrayList<Element>();
+	private TIntList a = new TIntLinkedList();
+	private TIntIntHashMap values = new TIntIntHashMap();
 	
 	public void insert(Node node, int value){
 		int p = a.size(); 
-		a.add(new Element(node, INF));
+		a.add(node.hashCode());
 		decreaseKey(p, value);
 	}
 	
 	public void decreaseKey(int index, int value){
-		assert(a.get(index).value < value);
-		a.get(index).value = value;
-		while(parent(index) != -1 && (a.get(parent(index)).value > value)){
-			Element temp = a.get(parent(index));
+		assert(values.get(a.get(index)) < value);
+		values.put(a.get(index), value);
+		while(parent(index) != -1 && (values.get(a.get(parent(index)))> value)){
+			int temp = a.get(parent(index));
 			a.set(parent(index), a.get(index));
 			a.set(index, temp);
 		}
@@ -28,38 +33,12 @@ public class BinaryMinHeap {
 		return -1;
 	}
 	
-	public Node extractMin(){
-		return a.remove(0).getNode();
+	// Actually returns the hashcode of the node
+	// If we want the actual node, we need to
+	// do a graph.getNode() on this key
+	public int extractMin(){
+		return a.removeAt(0);
 	}
 	
-	public static final class Element{
-		
-		private Node node;
-		
-		private int value;
-		
-		public Element(Node node, int value) {
-			super();
-			this.node = node;
-			this.value = value;
-		}
-		
-		public Node getNode() {
-			return node;
-		}
-		
-		public void setNode(Node node) {
-			this.node = node;
-		}
-		
-		public int getValue() {
-			return value;
-		}
-		
-		public void setValue(int value) {
-			this.value = value;
-		}
-		
-	}
 
 }
